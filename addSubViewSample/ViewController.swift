d7aba724bd6aae02c7814a62182f4f896d0e94ad
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-06-17 21:33:43 +0900

    完成

--- addSubViewSample/ViewController.swift
+++ addSubViewSample/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private var sampleView: UIView?
+    private var warabiAppendView = [UIImageView]()
+
+    var x = 0
+    var y = 0
+
+    @IBAction func addButton(_ sender: Any) {
+
+        let warabiImage = UIImage(named: "warabi.png")
+        let warabiImageView = UIImageView(image: warabiImage)
+        let rect = CGRect(x: x, y: y, width: 100, height: 100)
+
+        warabiImageView.frame = rect
+        self.view.addSubview(warabiImageView)
+        //warabiImageViewを格納する
+        warabiAppendView.append(warabiImageView)
+        //y軸を100ずつ下にずらす
+        y += 100
     }
-
-
+    
+    @IBAction func deleteActionButton(_ sender: Any) {
+        //warabiImageViewをaddSubviewしたのを一括で削除
+        for view in warabiAppendView {
+            view.removeFromSuperview()
+        }
+    }
 }
-
